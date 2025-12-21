7cf7cd195b57d1853fa6329a50a722ac71f4b57f
LuisitoRizado <laog.sonic4@gmail.com>
2025-12-20 19:10:36 -0600

    [Database] Added Mysql connector

--- src/main/java/com/luisitorizado/connection/mysql/MySqlConnector.java
+++ src/main/java/com/luisitorizado/connection/mysql/MySqlConnector.java
@@ -1,14 +1,33 @@
 package com.luisitorizado.connection.mysql;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 
+import com.luisitorizado.connection.DBConfig;
 import com.luisitorizado.connection.IDBConnection;
 
 public class MySqlConnector implements IDBConnection {
+    private final DBConfig config;
+
+    public MySqlConnector(DBConfig config) {
+        this.config = config;
+    }
 
     public Connection connect() {
-        IO.println("Logica conexion de mysql");
-
-        return null;
+        IO.println("Logica de conexion a postgres");
+        String connectionString = "jdbc:postgresql://" + this.config.getHost() + ":"
+                + Integer.toString(this.config.getPort())
+                + "/" + this.config.getDatabase();
+        try {
+            Connection conn = DriverManager.getConnection(connectionString, config.getUsername(), config.getPassword());
+            if (conn.isValid(100)) {
+                IO.println("Conectado!");
+                return conn;
+            } else
+                return null;
+        } catch (Exception e) {
+            IO.println(e);
+            return null;
+        }
     }
 }
